3bc7c64eccac145f527a03b89fa619e08a29cde2
ADVeg <danielveg2013@gmail.com>
2024-10-18 16:28:18 -0300

    V0.5

--- JS/JavaS.js
+++ JS/JavaS.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const btnCargar = document.querySelector('#btn-cargar');
     const inputExcel = document.querySelector('#input-excel');
     let archivoCargado = false;
+    let temp = [];
     let datos = [];
     let delitos = [['Causa', 'Cantidad']];
+    let info = [];
 
     //EVENTO CARGAR ARCHIVO
     inputExcel.addEventListener('change', (e) => {
@@ -25,8 +27,9 @@ document.addEventListener('DOMContentLoaded', function () {
             const sheet = workbook.Sheets[sheetName];
 
             const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-            rellenar(jsonData);
-            datos = jsonData;
+            temp = [];
+            pasarLimpio(jsonData);
+            
             archivoCargado = true; // Indica que se ha completado la lectura del archivo
         };
 
@@ -37,52 +40,82 @@ document.addEventListener('DOMContentLoaded', function () {
         reader.readAsBinaryString(file);
     })
 
-    function rellenar(jsonData) {
-        for (let index = 0; index < jsonData.length; index++) {
-            for (let index1 = 0; index1 < jsonData[index].length; index1++) {
-                if (jsonData[index][index1] === undefined) {
-                    jsonData[index][index1] = '-';
+    function completar() {
+        for (let i = 0; i < datos.length; i++) {
+            for (let p = 0; p < datos[i].length; p++) {
+                if (datos[i][p] === undefined) {
+                    datos[i][p] = '-';
                 }
             }
-        }
+        };
+    }
+
+    function pasarLimpio(jsonData) {   //completar tamaño tabla limpia
+        jsonData.forEach((row, i) => {
+            if (i===5) {
+                info[0]=row;
+            }
+            if (i === 6 || !isNaN(row[1])) {    ///recorrer array y tomar celda por celda si no contiene menos de 20 rellenar con -
+                if (row.length < 20) {
+                    for (let i = row.length; i < 20; i++) {
+                        row.push('-');
+                    };
+                };
+                temp.push(row);
+            }
+        });
+        
     }
 
     //BOTON CARGAR DATOS
     btnCargar.addEventListener('click', (e) => {
-        if (!archivoCargado) {
+        if (!archivoCargado || inputExcel.value==='') {  ///verificar que se elija un archivo
             alert('Elija un archivo');
             return;
         }
         alert('Archivo cargado correctamente');
-
-        cargarDatosTabla();
-        cargarTitulos(datos, 5, document.querySelector('#dataTable'));
-        cargarTitulos(delitos, 0, document.querySelector('#dataTable1'));
+        guardarDatos(temp);
+        
+        completar();
+        cargarDatosTabla(); ///cargamos datos tabla
+        cargarTitulos(info, document.querySelector('#dataTable'));  ///cargamos titulos datos
+        cargarTitulos(delitos, document.querySelector('#dataTable1'));   ///cargamos titulos dalitos
+        inputExcel.value='';
     })
 
+    function guardarDatos(temp) {
+        temp.forEach((row)=>{
+            datos.push(row);
+        });
+    }
+
     function cargarDatosTabla() {
-        // CARGAR LOS DATOS EN LA TABLA
+        //cargar tabla de datos
         const table = document.querySelector('#dataTable');
         const tbody = table.querySelector('tbody');
         tbody.innerHTML = '';
 
-        datos.slice(6).forEach((row) => {
+        datos.forEach((row) => {
             const tr = document.createElement('tr');
             row.forEach((cell, index) => {
                 if (!isNaN(row[1])) {
                     const td = document.createElement('td');
                     td.textContent = formatCell(cell, index); //VERIFICAR CELDA DE FECHA O HORA
                     tr.appendChild(td);
-
-                    if (index === 5 && cell !== '-') {  //CARGAR LISTA CAUSAS
-                        procesarCausa(cell);
-                    }
-
-                }
+                };
 
             });
             tbody.appendChild(tr);
         });
+
+        temp.forEach((row)=>{
+            row.forEach((cell,i)=>{
+                if (i === 5 && cell !== '-') {  //CARGAR LISTA CAUSAS
+                    procesarCausa(cell);
+                };
+            });
+        });
+
         cargarTablaDelitos();
     }
 
@@ -97,7 +130,7 @@ document.addEventListener('DOMContentLoaded', function () {
         return cell;
     }
 
-    function cargarTablaDelitos() {
+    function cargarTablaDelitos() { ///cargar tabla delito
         const tablaDelitos = document.querySelector('#dataTable1');
         const tbodyDelitos = tablaDelitos.querySelector('tbody');
         tbodyDelitos.innerHTML = '';
@@ -127,12 +160,12 @@ document.addEventListener('DOMContentLoaded', function () {
         delitos.push([causa, 1]);
     }
 
-    function cargarTitulos(Titulos, p, table) {
-        // Carga los títulos en la tabla
+    function cargarTitulos(Titulos, table) {
+        // Carga los títulos en la tabla correspondiente
         const thead = table.querySelector('thead');
         thead.innerHTML = '';
         const tr = document.createElement('tr');
-        Titulos[p].forEach((titulo) => {
+        Titulos[0].forEach((titulo) => {
             const th = document.createElement('th');
             th.textContent = titulo;
             tr.appendChild(th);
